4bb740003797377450db53c99662c8d6ed9af4b9
Dariusz Szybowski <dariusz.szybowski@aspediens.com>
2012-01-18 14:59:00 +0100

    Done with static pages

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,19 +1,51 @@
 require 'spec_helper'
 
 describe PagesController do
-
+  render_views
+  
   describe "GET 'home'" do
-    it "returns http success" do
+    # Test presence
+    it "should be successful" do
       get 'home'
       response.should be_success
     end
+        
+    # Test the title    
+    it "should have the right title" do
+      get 'home'
+      response.should have_selector("title", :content => "Dariusz Sample App | Home")
+    end
+    
   end
 
+  # Test presence
   describe "GET 'contact'" do
-    it "returns http success" do
+    it "should be successful" do
       get 'contact'
       response.should be_success
     end
+
+    # Test the title    
+    it "should have the right title" do
+      get 'contact'
+      response.should have_selector("title", :content => "Dariusz Sample App | Contact")
+    end
+
+  end
+
+  # Test presence
+  describe "GET 'about'" do
+    it "should be successful" do
+      get 'about'
+      response.should be_success
+    end
+
+    # Test the title    
+    it "should have the right title" do
+      get 'about'
+      response.should have_selector("title", :content => "Dariusz Sample App | About")
+    end
+
   end
 
 end
