d3feb9b9e2e0b56d2d05bc58d813b7364996c1f8
Dariusz Szybowski <dariusz.szybowski@aspediens.com>
2012-01-19 12:06:14 +0100

    "Finished layout and routes"

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 SampleApp::Application.routes.draw do
+
   # match '/about',   :to => 'pages#about'
   # match actually /about to the action "about" on pages_controller but also by default creates named ro
   # about_path => '/about'
@@ -7,6 +8,9 @@ SampleApp::Application.routes.draw do
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
   match '/help',    :to => 'pages#help'
+
+  match '/signup',  :to => 'users#new'
+  get "users/new"
 
   root :to => 'pages#home'
   
