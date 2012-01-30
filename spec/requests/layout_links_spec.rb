f753a0575269ae6e3150790d1d5e8fe62a27ea82
Dariusz Szybowski <dariusz.szybowski@aspediens.com>
2012-01-30 16:31:28 +0100

    Chapter 9 + exercise

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -61,11 +61,8 @@ describe "LayoutLinks" do
   describe "when signed in" do
     
     before(:each) do
-      @user = Factory(:user)
-      visit signin_path
-      fill_in :email,    :with => @user.email
-      fill_in :password, :with => @user.password
-      click_button
+      user = Factory(:user)
+      integration_sign_in(user)
     end
     
     it "should have a signout link" do
